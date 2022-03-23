0dbc4bcfd0bb33456808fd63647045faea51b4a2
pom <>
2022-03-23 10:54:53 +0100

    Add color, update css, scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
+const pluginPWA = require("eleventy-plugin-pwa");
+const pkg = require("./package.json");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksShortcode("version", function () {
+    return pkg.version;
+  });
   eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("manifest.webmanifest");
+  eleventyConfig.addPlugin(pluginPWA);
 };
